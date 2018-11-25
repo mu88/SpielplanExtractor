91d6886be0fbf4e89c2cad3ca1d64e3309a48d06
mu88 <mu88_developer@outlook.com>
2018-11-25 15:11:42 +0100

    Add business code

---
+++ SpielplanExtractor/SpielplanExtractor/Game.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SpielplanExtractor
+{
+    internal class Game
+    {
+        public DateTime Date { get; }
+        public string Location { get; }
+        public string Opponent { get; }
+        public string Identifier { get; }
+
+        public Game(DateTime dateTime, string location, string opponent, string identifier)
+        {
+            Date = dateTime;
+            Location = location;
+            Opponent = opponent;
+            Identifier = identifier;
+        }
+    }
+}
---
+++ SpielplanExtractor/SpielplanExtractor/Season.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace SpielplanExtractor
+{
+    internal class Season
+    {
+        public Season(string seasonString)
+        {
+            Games = new Collection<Game>();
+
+            var seasonStrings = seasonString.Split('/');
+            StartYear = Convert.ToInt32(seasonStrings[0]);
+            EndYear = Convert.ToInt32(seasonStrings[1]);
+        }
+
+        public int EndYear { get; }
+
+        public int StartYear { get; }
+
+        public Collection<Game> Games { get; }
+    }
+}
